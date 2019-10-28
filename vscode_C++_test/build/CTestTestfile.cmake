46e1346ba631c7a9d059d6adc0137851a977b07b
wenzhiyin <616122771@qq.com>
2019-10-28 16:22:16 +0800

    vscode + C++ + ros kinetic中的opencv3使用

---
+++ vscode_C++_test/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/indemind/Documents/github/talk_is_cheap/vscode_C++_test
+# Build directory: /home/indemind/Documents/github/talk_is_cheap/vscode_C++_test/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
---
+++ vscode_C++_test/readme.txt
@@ -0,0 +1,8 @@
+文档用于测试ubuntu下使用vscode进行C++开发
+
+这里使用了 opencv 库  这个库是包含在ROS kinect中 中的opencv3
+
+虽然在main函数中的包含文件显示找不到路径  但是编译运行是没有问题的
+
+参考：https://blog.csdn.net/yuguo0_331/article/details/82656568 
+参考：https://blog.csdn.net/u010284636/article/details/80071786 
