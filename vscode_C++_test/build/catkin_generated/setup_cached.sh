f67deacdeb221bf277d6837f3214c292b434f2d4
wenzhiyin <616122771@qq.com>
2019-10-28 16:22:16 +0800

    vscode + C++ + ros kinetic中的opencv3使用

---
+++ vscode_C++_test/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/indemind/Documents/github/talk_is_cheap/vscode_C++_test/build/devel:$CMAKE_PREFIX_PATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/indemind/Documents/github/talk_is_cheap/vscode_C++_test/build/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/indemind/Documents/github/talk_is_cheap/vscode_C++_test:$ROS_PACKAGE_PATH"
